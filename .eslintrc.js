f4570c417f030464fa0cb161e2a4991452760724
Henry <hlewis@uchicago.edu>
2018-03-24 10:08:30 -0500

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    extends: ['prettier'],
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: 'eslint:recommended',
-    rules: {
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'never'],
-    },
-}
+  extends: ["prettier"],
+  env: {
+    es6: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "never"]
+  }
+};
