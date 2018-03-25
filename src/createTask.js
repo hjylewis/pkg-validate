664a20b60940b90364280775fb86160c09563b5b
Henry <hlewis@uchicago.edu>
2018-03-24 23:37:56 -0500

    WIP status using observables

--- src/createTask.js
+++ src/createTask.js
@@ -1,5 +1,6 @@
 const execa = require('execa');
 const getCommand = require('./getCommand');
+const { Observable } = require('rxjs/Observable');
 
 module.exports = function createTask(command) {
   const { bin, args } = getCommand(command);
@@ -7,7 +8,31 @@ module.exports = function createTask(command) {
   return {
     title: command,
     task: () => {
-      return execa(bin, args);
+      const e = execa(bin, args);
+
+      const error = new Observable(observer => {
+        const onData = data => {
+          const trimmed = data.toString('utf8').replace(/^\s+|\s+$/g, '');
+          const path = trimmed.split(/\r?\n/);
+          observer.next(path[path.length - 1]);
+        };
+
+        e.stdout.on('data', onData);
+        e.stderr.on('data', onData);
+
+        e
+          .then(() => {
+            // e.stdout.removeListener('data', onData);
+            observer.complete();
+          })
+          .catch(err => {
+            // e.stdout.removeListener('data', onData);
+            observer.error(err);
+            observer.complete();
+          });
+      });
+
+      return error;
     }
   };
 };
